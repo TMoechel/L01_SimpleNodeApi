4f7c96110efa62401cbbf1e3f5fc83856445f81a
Möchel <T.Moechel@FS.local>
2018-07-27 07:01:23 +0200

    Initial Checkin

---
+++ server.js
@@ -0,0 +1,25 @@
+// TODO: create a basic server with express
+// that will send back the index.html file on a GET request to '/'
+// it should then send back jsonData on a GET to /data
+var express = require('express');
+var app = express();
+
+var jsonData = {count: 12, message: 'Welcome to Node'};
+
+app.get('/', function(req,res) {
+    res.sendFile(__dirname + '/index.html', function(err){
+        if (err) {
+            res.status(500).send(err);
+        }
+    })
+});
+
+app.get('/data', function(req,res){
+    res.json(jsonData);
+})
+
+var port = 3000;
+
+app.listen(port);
+
+console.log('node server is running on port 3000');
