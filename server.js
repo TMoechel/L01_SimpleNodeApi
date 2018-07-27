70ebfec4a01e683db236994c25b6df9d7d00750c
TMoechel <t.moechel@softcreation.de>
2018-07-27 18:55:11 +0200

    Update to Rest Structure Exercise

--- server.js
+++ server.js
@@ -2,20 +2,33 @@
 // that will send back the index.html file on a GET request to '/'
 // it should then send back jsonData on a GET to /data
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
+var _ = require('lodash');
 
-var jsonData = {count: 12, message: 'Welcome to Node'};
+var PersonList = [ {
+    "ID" : 1,
+    "FirstName" : "John",
+    "LastName" : "Miller",
+    "Age" : 34
+}];
 
-app.get('/', function(req,res) {
-    res.sendFile(__dirname + '/index.html', function(err){
-        if (err) {
-            res.status(500).send(err);
-        }
-    })
-});
+//HTTP Verbs GET, POST (Insert), PUT (Update), DELETE
 
-app.get('/data', function(req,res){
-    res.json(jsonData);
+// app.get('/', function(req,res) {
+//     res.sendFile(__dirname + '/index.html', function(err){
+//         if (err) {
+//             res.status(500).send(err);
+//         }
+//     })
+// });
+
+app.use(express.static(__dirname));
+app.use(bodyParser.urlencoded( {extended: true}));
+app.use(bodyParser.json());
+
+app.get('/personList', function(req,res){
+    res.json(PersonList);
 })
 
 var port = 3000;
